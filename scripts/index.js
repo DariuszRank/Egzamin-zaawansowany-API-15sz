56ac2036bde2ab587fdf0aa168f7d5d2dd417776
DariuszRank <rank_d@onet.eu>
2023-02-26 21:21:47 +0100

    zadanie egzaminacyjne

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+alert('Dariusz Rank'); // W ten sposób mogę wywołać swoje imię jako okienko na ekranie
+document.write('Dariusz Rank') // - w ten sposób mogę wypisać swoje imię i nazwisko na ekranie pod artykułem
+document.getElementById("name").innerHTML = "Dariusz"; // a w ten sposób nadpisać obecne nagłówki imieniem
+document.getElementById("lastname").innerHTML = "Rank"; // i nazwiskiem
